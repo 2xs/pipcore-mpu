46a50ae259ae9e855d2b7b8a7daa72b5e4a9b64e
Nicolas Dejon <nicolas.dejon@orange.com>
2021-04-29 10:48:41 +0200

    Board: avoid multiple debug header declaration

--- src/boot/dwm1001/ARM/debug/pip_debug.h
+++ src/boot/dwm1001/ARM/debug/pip_debug.h
@@ -1,6 +1,11 @@
+#ifndef __PIP_DEBUG__
+#define __PIP_DEBUG__
+
 #include <stdio.h>
 
 void dump_PD_structure(paddr pd);
 void dump_kernel_structure(paddr kernel_structure_start_addr);
 void dump_partition(paddr part);
 void dump_ancestors(paddr base_child_PD_address);
+
+#endif // __PIP_DEBUG__
