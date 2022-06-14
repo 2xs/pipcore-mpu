3db590d4bad607a29c0b6ed706dc89ed6dd6f4b4
Samuel Hym <samuel.hym@univ-lille.fr>
2022-06-14 20:15:39 +0200

    Remove CI configuration (gcc too old)
    gcc version on the Coq docker image is too old, it gives useless errors
    when compiling the C code

--- .ci/apt-get.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# "apt update ; apt install", but from the local mirror
-
-sudo cp .ci/sources.list /etc/apt/sources.list
-sudo apt update -yq
-sudo DEBIAN_FRONTEND=noninteractive apt install -yq --no-install-recommends "$@"
