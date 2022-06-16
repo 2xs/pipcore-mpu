d4814469767d46935515ad17b1bda236b9f29617
Samuel Hym <samuel.hym@univ-lille.fr>
2022-06-16 18:16:57 +0200

    Start dx configuration

---
+++ src/dx/Defs.v
@@ -0,0 +1,269 @@
+(*******************************************************************************)
+(*  © Université de Lille, The Pip Development Team (2015-2022)                *)
+(*                                                                             *)
+(*  This software is a computer program whose purpose is to run a minimal,     *)
+(*  hypervisor relying on proven properties such as memory isolation.          *)
+(*                                                                             *)
+(*  This software is governed by the CeCILL license under French law and       *)
+(*  abiding by the rules of distribution of free software.  You can  use,      *)
+(*  modify and/ or redistribute the software under the terms of the CeCILL     *)
+(*  license as circulated by CEA, CNRS and INRIA at the following URL          *)
+(*  "http://www.cecill.info".                                                  *)
+(*                                                                             *)
+(*  As a counterpart to the access to the source code and  rights to copy,     *)
+(*  modify and redistribute granted by the license, users are provided only    *)
+(*  with a limited warranty  and the software's author,  the holder of the     *)
+(*  economic rights,  and the successive licensors  have only  limited         *)
+(*  liability.                                                                 *)
+(*                                                                             *)
+(*  In this respect, the user's attention is drawn to the risks associated     *)
+(*  with loading,  using,  modifying and/or developing or reproducing the      *)
+(*  software by the user in light of its specific status of free software,     *)
+(*  that may mean  that it is complicated to manipulate,  and  that  also      *)
+(*  therefore means  that it is reserved for developers  and  experienced      *)
+(*  professionals having in-depth computer knowledge. Users are therefore      *)
+(*  encouraged to load and test the software's suitability as regards their    *)
+(*  requirements in conditions enabling the security of their systems and/or   *)
+(*  data to be ensured and,  more generally, to use and operate it in the      *)
+(*  same conditions as regards security.                                       *)
+(*                                                                             *)
+(*  The fact that you are presently reading this means that you have had       *)
+(*  knowledge of the CeCILL license and that you accept its terms.             *)
+(*******************************************************************************)
+
+From Coq Require Import ZArith.
+From Coq Require Import List String.
+Import ListNotations.
+
+Close Scope nat_scope.
+Open Scope Z_scope.
+
+From compcert.cfrontend Require Csyntax Ctypes Cop.
+From compcert.common Require Values.
+From compcert.lib Require Integers.
+
+From dx      Require Import CoqIR IR IRtoC ResultMonad.
+From dx.Type Require Bool Nat.
+
+Import UserIdentNotations.
+
+From Model Require Monad ADT MALInternal MAL.
+From Core  Require Internal Services.
+
+Definition uint32 : Ctypes.type :=
+  Ctypes.Tint Ctypes.I32 Ctypes.Unsigned Ctypes.noattr.
+
+Module PipTypes.
+  Definition paddrCompilableType := MkCompilableType ADT.paddr uint32.
+  Definition indexCompilableType := MkCompilableType ADT.index uint32.
+End PipTypes.
+
+Definition funPaddrPaddrBoolType :=
+  MkCompilableSymbolType
+    [ PipTypes.paddrCompilableType; PipTypes.paddrCompilableType ]
+    (Some Bool.boolCompilableType).
+
+Definition funIndexIndexBoolType :=
+  MkCompilableSymbolType
+    [ PipTypes.indexCompilableType; PipTypes.indexCompilableType ]
+    (Some Bool.boolCompilableType).
+
+(* Definition cval n := *)
+(*   Csyntax.Eval (Values.Vint (Integers.Int.repr n)) DataTypes.constantsT. *)
+
+Definition cBinOp o es :=
+  match es with
+  | [e1;e2] => Ok (Csyntax.Ebinop o e1 e2 Ctypes.type_bool)
+  | _       => Err PrimitiveEncodingFailed
+  end.
+
+Definition cEq := cBinOp Cop.Oeq.
+(* Definition cGe := cBinOp Cop.Oge. *)
+(* Definition cGt := cBinOp Cop.Ogt. *)
+(* Definition cLe := cBinOp Cop.Ole. *)
+(* Definition cLt := cBinOp Cop.Olt. *)
+
+Module PipPrimitives.
+  Definition paddrEqPrim := MkPrimitive funPaddrPaddrBoolType MALInternal.beqAddr cEq.
+  Definition indexEqPrim := MkPrimitive funIndexIndexBoolType MALInternal.beqIdx cEq.
+
+(*   Definition idxGePrim   := MkPrimitive funIndexIndexBoolType Ops.idxGe   cGe. *)
+(*   Definition idxGtPrim   := MkPrimitive funIndexIndexBoolType Ops.idxGt   cGt. *)
+(*   Definition idxLePrim   := MkPrimitive funIndexIndexBoolType Ops.idxLe   cLe. *)
+(*   Definition idxLtPrim   := MkPrimitive funIndexIndexBoolType Ops.idxLt   cLt. *)
+(*   Definition vaddrEqPrim := MkPrimitive funVAddrVAddrBoolType Ops.vaddrEq cEq. *)
+(*   Definition pageEqPrim  := MkPrimitive funPagePageBoolType   Ops.pageEq  cEq. *)
+(*   Definition levelEqPrim := MkPrimitive funLevelLevelBoolType Ops.levelEq cEq. *)
+(*   Definition levelGtPrim := MkPrimitive funLevelLevelBoolType Ops.levelGt cGt. *)
+End PipPrimitives.
+
+GenerateIntermediateRepresentation
+  InternalHIRSyms
+  Monad.LLI Monad.bind Monad.ret
+
+  Bool.Exports
+  Nat.Exports
+
+  PipTypes
+
+  Internal
+  .
+
+Definition dxModuleInternalH := makeDXModuleWithDefaults InternalHIRSyms.
+
+GenerateIntermediateRepresentation
+  InternalIRSyms
+  Monad.LLI Monad.bind Monad.ret
+
+  Bool.Exports
+  Nat.Exports
+
+  PipTypes
+  PipPrimitives
+
+  MALInternal
+  MAL
+
+  (* Cannot be converted by dx, should be moved in another module *)
+  Internal.N
+  __
+
+  (* Internal.N *)
+  Internal.compareAddrToNull
+  Internal.findBlockComp
+  Internal.findBlockInKSInStructAux
+  Internal.findBlockInKSAux
+  Internal.findBlockInKS
+  Internal.findBelongingBlock
+  Internal.findBlockInKSWithAddrAux
+  Internal.findBlockInKSWithAddr
+  Internal.checkBlockCut
+  Internal.writeAccessibleRecAux
+  Internal.writeAccessibleRec
+  Internal.writeAccessibleToAncestorsIfNotCutRec
+  Internal.insertNewEntry
+  Internal.freeSlot
+  Internal.checkChildOfCurrPart
+  Internal.removeBlockInDescendantsRecAux
+  Internal.removeBlockInDescendantsRec
+  Internal.checkRemoveSubblocksRecAux
+  Internal.checkRemoveSubblocksRec
+  Internal.removeSubblocksRecAux
+  Internal.removeSubblocksRec
+  Internal.removeBlockInChildAndDescendants
+  Internal.sizeOfBlock
+  Internal.initBlockEntryRecAux
+  Internal.initBlocksStructure
+  Internal.initSh1EntryRecAux
+  Internal.initSh1Structure
+  Internal.initSCEntryRecAux
+  Internal.initSCStructure
+  Internal.initStructure
+  Internal.deleteSharedBlocksInStructRecAux
+  Internal.deleteSharedBlocksRecAux
+  Internal.deleteSharedBlocksRec
+  Internal.removeStructure
+  Internal.checkStructureEmptyRecAux
+  Internal.collectFreeSlotsRecAux
+  Internal.collectStructureRecAux
+  Internal.collectStructureRec
+  Internal.enableBlockInMPU
+  Internal.removeBlockFromPhysicalMPUIfAlreadyMapped
+  Internal.getGlobalIdPDCurrentOrChild
+  Internal.compatibleRight
+  Internal.checkRights
+  .
+
+Definition dxModuleInternal := makeDXModuleWithDefaults InternalIRSyms.
+
+(* GenerateIntermediateRepresentation *)
+(*   ServicesHIRSyms *)
+(*   Monad.LLI Monad.bind Monad.ret *)
+
+(*   Bool.Exports *)
+(*   Nat.Exports *)
+
+(*   DataTypes.Exports *)
+(*   PipPrimitives *)
+
+(*   Constants *)
+
+(*   Ops.idxPredM *)
+(*   Ops.idxSuccM *)
+(*   Ops.levelPredM *)
+(*   Ops.levelSuccM *)
+(*   Ops.countSuccM *)
+(*   Ops.countFromLevelM *)
+
+(*   MAL *)
+
+(*   IAL.setInterruptMask *)
+(*   IAL.getInterruptMaskFromCtx *)
+(*   IAL.noInterruptRequest *)
+(*   IAL.writeContext *)
+(*   IAL.vaddrToContextAddr *)
+(*   IAL.checkIndexPropertyLTB *)
+(*   IAL.userValueToIndex *)
+(*   IAL.updateMMURoot *)
+
+(*   ADT.nbLevel *)
+(*   ADT.tableSize *)
+(*   ADT.nbPage *)
+(*   ADT.maxVint *)
+(*   ADT.contextSize *)
+
+(*   Services *)
+(*   . *)
+
+(* Definition dxModuleServicesH := makeDXModuleWithUserIds DataTypes.composites DataTypes.structIds ServicesHIRSyms. *)
+
+(* GenerateIntermediateRepresentation *)
+(*   ServicesIRSyms *)
+(*   Monad.LLI Monad.bind Monad.ret *)
+
+(*   Bool.Exports *)
+(*   Nat.Exports *)
+
+(*   DataTypes.Exports *)
+(*   PipPrimitives *)
+
+(*   Constants *)
+
+(*   Ops.idxPredM *)
+(*   Ops.idxSuccM *)
+(*   Ops.levelPredM *)
+(*   Ops.levelSuccM *)
+(*   Ops.countSuccM *)
+(*   Ops.countFromLevelM *)
+
+(*   MAL *)
+
+(*   IAL.setInterruptMask *)
+(*   IAL.getInterruptMaskFromCtx *)
+(*   IAL.noInterruptRequest *)
+(*   IAL.writeContext *)
+(*   IAL.vaddrToContextAddr *)
+(*   IAL.checkIndexPropertyLTB *)
+(*   IAL.userValueToIndex *)
+(*   IAL.loadContext *)
+(*   IAL.getNthVAddrFrom *)
+(*   IAL.contextSizeMinusOne *)
+(*   IAL.firstVAddrGreaterThanSecond *)
+(*   IAL.updateMMURoot *)
+
+(*   ADT.nbLevel *)
+(*   ADT.boundNbLevel *)
+(*   ADT.tableSize *)
+(*   ADT.nbPage *)
+(*   ADT.boundNbPages *)
+(*   ADT.maxVint *)
+(*   ADT.contextSize *)
+
+(*   Internal *)
+
+(*   __ *)
+
+(*   Services *)
+(*   . *)
+
+(* Definition dxModuleServices := makeDXModuleWithUserIds DataTypes.composites DataTypes.structIds ServicesIRSyms. *)
